88f819f9f266a5c2ad5e34eb42171ac8a38f0f6f
Peter Koverda <peter@pulsegrow.com>
2024-04-23 16:38:29 -0700

    delete, and index link on article pg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,13 @@ class ArticlesController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
